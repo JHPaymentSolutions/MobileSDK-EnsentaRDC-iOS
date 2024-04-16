e6d26a191a61c153f2b25a833624d93a555aeb0a
ReemaB-JHA <rbhalani@jhacorp.com>
2024-04-16 10:58:48 -0700

    JPAY-1695 - Update swift.package file
    - Add second network layer framework.
    - Update package name.
    - Update checksum values for both binaries and update tag.

--- Package.swift
+++ Package.swift
@@ -4,20 +4,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "JackHenryMobileSDK-EnsentaRDC",
+    name: "JackHenryMobileSDK",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "JackHenryMobileSDK-EnsentaRDC",
+            name: "EnsentaRDC",
             targets: ["EnsentaRDC"]),
+        .library(
+            name: "EnsentaRDCNetwork",
+            targets: ["EnsentaRDCNetwork"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "EnsentaRDC",
-            url: "https://github.com/JHPaymentSolutions/MobileSDK-EnsentaRDC-iOS/releases/download/1.0.0/JackHenryMobileSDK-EnsentaRDC-1.0.0.xcframework.zip",
-            checksum: "cace51f472645557895e07c7ec4aff3ae055b63f849e854160f5d32fd3defbc5"
+            url: "https://github.com/JHPaymentSolutions/MobileSDK-EnsentaRDC-iOS/releases/download/1.1.0/JackHenryMobileSDK-EnsentaRDC.xcframework.zip",
+            checksum: "bf6f6b68fee231001aab0c809e5a293f3a493672539351e4c4202c57bc4dd3c7"
+        ),
+        .binaryTarget(
+            name: "EnsentaRDCNetwork",
+            url: "https://github.com/JHPaymentSolutions/MobileSDK-EnsentaRDC-iOS/releases/download/1.1.0/JackHenryMobileSDK-EnsentaRDCNetwork.xcframework.zip",
+            checksum: "11154eb96570bf7b19815808417bb5c7d4beb9c76a5b26a9cd4121d3d2812069"
         ),
     ]
 )
